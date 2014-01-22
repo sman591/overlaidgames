bbf5a1d60210ec668cddd59e0d260ed3b4627217
Stuart Olivera <stuart@stuartolivera.com>
2014-01-22 00:20:28 -0500

    Added bootstrap

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ gem "jquery-rails"
 gem "mysql2"
 gem "sass-rails", "~> 4.0.0"
 gem "uglifier", ">= 1.3.0"
+
+gem 'bootstrap-sass', '~> 3.0.3.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery
+//= require bootstrap
 //= require handlebars
 //= require ember
 //= require ember-data
