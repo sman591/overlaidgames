f54c90bb97f8d4ea5670ee8be9889d878a339b01
Stuart Olivera <stuart@stuartolivera.com>
2014-01-21 23:19:36 -0500

    Bootstrapped Ember.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require overlaygames
+
+// for more details see: http://emberjs.com/guides/application/
+Overlaygames = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Overlaygames.Router.map(function() {
+  // this.resource('posts');
+});
