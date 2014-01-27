bf8fa67bb34bda133a0a2f670cacb9b7ff8175de
Stuart Olivera <stuart@stuartolivera.com>
2014-01-26 23:21:43 -0500

    Fixed routing for backgrounds
    Path ID had not been changed from "overlay" to "background"

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,7 +3,7 @@ Overlaidgames.Router.map(function() {
     this.resource("game", { path: ":game_id" });
   });
   this.resource("backgrounds", function() {
-    this.resource("background", { path: ":overlay_id" });
+    this.resource("background", { path: ":background_id" });
   });
   this.resource("about");
   this.resource("request");
