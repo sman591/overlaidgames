5d07d8a8f254fa8b267e5e61ca096bff4e15f279
Stuart Olivera <stuart@stuartolivera.com>
2014-01-26 23:22:28 -0500

    Added full background support

--- app/assets/javascripts/models/background.js
+++ app/assets/javascripts/models/background.js
@@ -5,5 +5,15 @@ Overlaidgames.Background = DS.Model.extend({
   slug: DS.attr('string'),
   src: DS.attr('string'),
   uses: DS.attr('number'),
-  enabled: DS.attr('boolean')
+  enabled: DS.attr('boolean'),
+  isWebsite: function() {
+    var website_regex = /^(http(s?):\/\/)/;
+    return website_regex.test(this.get('src'));
+  }.property('src'),
+  divStyle: function() {
+    var website_regex = /^(http(s?):\/\/)/;
+    if (website_regex.test(this.get('src')))
+      return '';
+    return "background-image: url('http://belmontridgemiddleschool.com/bg/" + this.get('src') + "')";
+  }.property('src')
 });
